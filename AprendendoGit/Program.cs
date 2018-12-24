66d31493eda574f74678aee273d27d7752bc35d6
Eduardo <eduardopenshorn@gmail.com>
2018-12-24 12:36:20 -0200

    acrescentei 5 la e 5 lo

--- AprendendoGit/Program.cs
+++ AprendendoGit/Program.cs
@@ -8,6 +8,7 @@ namespace AprendendoGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Linha 2. Aqui é onde eu falo bom dia pessoal, tudo bem com vocês?");
+            Console.WriteLine("lalalalalalololololo");
             Console.ReadLine();
         }
     }
