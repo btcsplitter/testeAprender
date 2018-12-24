aead273c8db482d7401c0401dc3bd647fe07e601
Eduardo <eduardopenshorn@gmail.com>
2018-12-24 10:02:03 -0200

    agora tem bom dia

--- AprendendoGit/Program.cs
+++ AprendendoGit/Program.cs
@@ -7,6 +7,7 @@ namespace AprendendoGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Linha 2. Aqui é onde eu falo bom dia pessoal, tudo bem com vocês?");
             Console.ReadLine();
         }
     }
