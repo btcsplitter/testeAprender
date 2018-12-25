4fe16845349c5cf1ff6a2e723c65a645c86a6f0f
Eduardo <eduardopenshorn@gmail.com>
2018-12-24 22:17:21 -0200

    substitui 5la5lo por varios li

--- AprendendoGit/Program.cs
+++ AprendendoGit/Program.cs
@@ -8,7 +8,7 @@ namespace AprendendoGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Linha 2. Aqui é onde eu falo bom dia pessoal, tudo bem com vocês?");
-            Console.WriteLine("lalalalalalololololo");
+            Console.WriteLine("lilililililililililililili");
             Console.ReadLine();
         }
     }
